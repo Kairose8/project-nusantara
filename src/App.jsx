e7c4a9e2b8f12306114daf668c72f3e970b13542
Kairose8 <hafizhdikara@gmail.com>
2024-05-08 22:29:50 +0700

    init
    pindah file local ke git

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Navbar from "./compnents/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
---
+++ src/pages/AboutUS.jsx
@@ -0,0 +1,9 @@
+import desi from "../assets/desi.jpg";
+import sharon from "../assets/sharon.jpg";
+import tere from "../assets/tere.jpg";
+
+export default function AboutUS(){
+    return(
+
+    );
+}

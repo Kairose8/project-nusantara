33f8969cb0436c3f1c0c22bf26e84caf9772cbd5
denjelll <lumenondenzel@gmail.com>
2024-05-10 19:14:11 +0700

    tambah wallpaper sama kata map

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -2,7 +2,7 @@ export default function MapPage() {
 
     return (
       <>
-        <h1>Map</h1>
+        <h1 class="text-5xl mt-10 mb-10 text-center text-amber-400 font-extrabold">Map</h1>
       </>
     );
   }

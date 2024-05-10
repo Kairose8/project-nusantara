563e81c87fbc148e2c0ec71503a033c4065ba35a
Kairose8 <hafizhdikara@gmail.com>
2024-05-11 03:01:46 +0700

    nambah beberapa page
    isi page dengan styling sementara

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -3,7 +3,16 @@ export default function ExplorePage(){
 
     return(
         <>
-            <h1>Explore Page</h1>
+            <>
+      <div class="absolute top-96 left-14 font-serif text-xl text-white  w-1/3">
+        <p>Only in</p>
+      </div>
+        <div class="absolute bottom-96 left-14 font-serif text-5xl font-bold text-white  w-1/3 ">
+          <p class="text-s">Yogyakarta</p>
+        </div>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+    </>
         </>
     );
 }

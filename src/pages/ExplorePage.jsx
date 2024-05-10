b9b7c960f5d9ee066096f7d7afca5f92de454383
Kairose8 <hafizhdikara@gmail.com>
2024-05-11 03:08:51 +0700

    Update ExplorePage.jsx

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -7,8 +7,8 @@ export default function ExplorePage(){
       <div class="absolute top-96 left-14 font-serif text-xl text-white  w-1/3">
         <p>Only in</p>
       </div>
-        <div class="absolute bottom-96 left-14 font-serif text-5xl font-bold text-white  w-1/3 ">
-          <p class="text-s">Yogyakarta</p>
+        <div class="absolute top-96 left-14 font-serif text-5xl font-bold text-white  w-1/3 ">
+          <p class="text-s"><br/>Yogyakarta</p>
         </div>
       <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 

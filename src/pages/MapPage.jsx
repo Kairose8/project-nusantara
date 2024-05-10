36a379d9f0cf2b043c421a9abb4bcd0d1ba26697
denjelll <lumenondenzel@gmail.com>
2024-05-11 04:22:03 +0700

    menambahkan gambar ke dalam adventure map

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,12 +1,17 @@
+import map1 from "../assets/MAP.png";
 export default function MapPage() {
   return (
     <>
         <br />
-        <br />
-        <br />
         <p class="font-serif text-5xl mb-5 mt-52 text-white ml-12 w-1/3">
           Adventure Map
         </p>
+        <img class="w-150 h-80" src={map1}
+          style={{
+            position: "absolute",
+            right: `${140}px`,
+            top: `${140}px`,
+          }} />
         <br />
         <br />
         <br />        
@@ -16,7 +21,6 @@ export default function MapPage() {
         <br />
         <br />
         <br />
-
     </>
   );
 }

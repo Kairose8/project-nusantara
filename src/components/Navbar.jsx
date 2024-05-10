a5f2deb7af12da911cd306a597eaa4d3c0cf9a6d
Kairose8 <hafizhdikara@gmail.com>
2024-05-10 12:44:08 +0700

    Buat page about us

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -247,9 +247,7 @@ function Navbar() {
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <button onClick={() => navigate('/') }
-                  href="#"
-                  className="block py-2 px-3 text-white bg-yellow-600 rounded md:bg-transparent md:text-yellow-600 md:p-0 md:dark:text-yellow-600"
-                  aria-current="page"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-yellow-600 dark:text-white md:dark:hover:text-yellow-600 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Home
                 </button>
--- src/pages/AboutUS.jsx
+++ src/pages/AboutUS.jsx
@@ -1,13 +1,151 @@
 import desi from "../assets/desi.jpg";
 import sharon from "../assets/sharon.jpg";
 import tere from "../assets/tere.jpg";
+import bintang from "../assets/Bintang2.jpg";
+import denzel from "../assets/denzel1.jpg";
 
-export default function AboutUS(){
-    return(
-        <>
-            <h1>About Us</h1>
+export default function AboutUS() {
+  return (
+    <>
+      <h1 class="text-5xl mt-10 mb-10 text-center text-yellow-800 font-extrabold">
+        Hihihaha
+        <br/>
+        <small class="ms-2 font-semibold text-yellow-400 ">
+          Meet Our Team!
+        </small>
+      </h1>
 
-        </>
+      <div class="flex justify-around flex-wrap px-14">
+        <div class="w-52 mx-auto overflow-hidden bg-white rounded-lg shadow-lg hover:shadow-yellow-600 mb-5">
+          <div class="relative">
+            <img class="w-full h-48 object-cover" src={bintang} />
+          </div>
+          <div class="px-6 py-4">
+            <div class="text-xl font-semibold text-gray-800">
+              Hafizh Dikara Bintang
+            </div>
+            <p class="text-gray-600">00000104984</p>
+            <br />
+          </div>
+          <div class="px-6 py-4">
+            <span class="inline-block px-2 py-1 font-semibold text-zinc-50 bg-yellow-500 rounded-full ">
+              Code
+            </span>
+          </div>
+          <div class="px-6 py-4">
+            <a href="https://www.instagram.com/dikarabintang/" class="text-yellow-600 hover:underline">
+              @dikarabintang
+            </a>
+          </div>
+        </div>
 
-    );
+        <div class="w-52 mx-auto overflow-hidden bg-white rounded-lg shadow-lg hover:shadow-yellow-600 mb-5">
+          <div class="relative">
+            <img
+              class="w-full h-48 object-cover"
+              src={denzel}
+              alt="Profile Image"
+            />
+          </div>
+          <div class="px-6 py-4 pb-3">
+            <div class="text-xl font-semibold text-gray-800">
+              Benediktus Denzel Galgani Lumenon
+            </div>
+            <p class="text-gray-600">00000104984</p>
+          </div>
+          <div class="px-6 py-4 pb-3">
+            <span class="inline-block px-2 py-1 font-semibold text-zinc-50 bg-yellow-500 rounded-full ">
+              Code
+            </span>
+          </div>
+          <div class="px-6 py-4 pb-3">
+            <a href="https://www.instagram.com/denzellumenon/" class="text-yellow-600 hover:underline">
+              @denzellumenon
+            </a>
+          </div>
+        </div>
+
+        <div class="w-52 mx-auto overflow-hidden bg-white rounded-lg shadow-lg hover:shadow-yellow-600 mb-5">
+          <div class="relative">
+            <img
+              class="w-full h-48 object-cover"
+              src={desi}
+              alt="Profile Image"
+            />
+          </div>
+          <div class="px-6 py-4">
+            <div class="text-xl font-semibold text-gray-800">Descellia Ng</div>
+            <p class="text-gray-600">00000104984</p>
+            <br />
+            <br />
+          </div>
+          <div class="px-6 py-4 ">
+            <span class="inline-block px-2 py-1 font-semibold text-zinc-50 bg-yellow-700 rounded-full">
+              Design
+            </span>
+          </div>
+          <div class="px-6 py-4 ">
+            <a href="https://www.instagram.com/descellia/" class="text-yellow-600 hover:underline">
+              @descellia
+            </a>
+          </div>
+        </div>
+
+        <div class="w-52 mx-auto overflow-hidden bg-white rounded-lg shadow-lg hover:shadow-yellow-600 mb-5">
+          <div class="relative">
+            <img
+              class="w-full h-48 object-cover"
+              src={tere}
+              alt="Profile Image"
+            />
+          </div>
+          <div class="px-6 py-4 ">
+            <div class="text-xl font-semibold text-gray-800">
+              Theresia Glory Emmanuella
+            </div>
+            <p class="text-gray-600">00000104984</p>
+            <br />
+          </div>
+          <div class="px-6 py-4 ">
+            <span class="inline-block px-2 py-1 font-semibold text-zinc-50 bg-amber-800 rounded-full">
+              Outfit
+            </span>
+          </div>
+          <div class="px-6 py-4 ">
+            <a href="https://www.instagram.com/abigailtheresiaaa/" class="text-yellow-600 hover:underline">
+              @abigailtheresiaaa
+            </a>
+          </div>
+        </div>
+
+        <div class="w-52 mx-auto overflow-hidden bg-white rounded-lg shadow-lg hover:shadow-yellow-600 mb-5">
+          <div class="relative">
+            <img
+              class="w-full h-48 object-cover"
+              src={sharon}
+              alt="Profile Image"
+            />
+          </div>
+          <div class="px-6 py-4 ">
+            <div class="text-xl font-semibold text-gray-800">
+              Sharon Tionardi
+            </div>
+            <p class="text-gray-600">00000104984</p>
+            <br />
+            <br />
+          </div>
+          <div class="px-6 py-4 ">
+            <span class="inline-block px-2 py-1 font-semibold text-zinc-50 bg-amber-800 rounded-full">
+              Outfit
+            </span>
+          </div>
+          <div class="px-6 py-4 ">
+            <a href="https://www.instagram.com/shrron__/" class="text-yellow-600 hover:underline">
+              @shrron__
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }

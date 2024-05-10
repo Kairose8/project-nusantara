078a3a18528bdcc6ff6b7d0229af4573121c7c56
Kairose8 <hafizhdikara@gmail.com>
2024-05-11 04:50:21 +0700

    betulin posisi page map
    sementara

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -3,18 +3,18 @@ export default function MapPage() {
   return (
     <>
         <br />
+            <br />
+            <br />
+            <br />        
         <p class="font-serif text-5xl mb-5 mt-52 text-white ml-12 w-1/3">
           Adventure Map
         </p>
-        <img class="w-150 h-80" src={map1}
+        <img class="w-96 h-72" src={map1}
           style={{
             position: "absolute",
             right: `${140}px`,
-            top: `${140}px`,
+            top: `${300}px`,
           }} />
-        <br />
-        <br />
-        <br />        
         <br />
         <br />
         <br />

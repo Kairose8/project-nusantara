b1451d68efed6d91f487c870ee486dc3285f8b3c
denjelll <lumenondenzel@gmail.com>
2024-05-13 01:30:19 +0700

    pasang map API
    baru pasang apinya, tinggal tambah koordinat sama tinggal rapihin posisinya, sama hapus gambar map yang ga guna itu

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,26 +1,28 @@
-import map1 from "../assets/MAP.png";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer } from "react-leaflet";
+
 export default function MapPage() {
   return (
     <>
-        <br />
-        <p class="font-serif text-5xl mb-5 mt-52 text-white ml-12 w-1/3">
-          Adventure Map
-        </p>
-        <img class="w-150 h-80" src={map1}
-          style={{
-            position: "absolute",
-            right: `${140}px`,
-            top: `${140}px`,
-          }} />
-        <br />
-        <br />
-        <br />        
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+      <MapContainer center={[-6.2562185, 106.6159875, 17]} zoom={15}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+      <br />
+      <p class="font-serif text-5xl mb-5 mt-52 text-white ml-12 w-1/3">
+        Adventure Map
+      </p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 }

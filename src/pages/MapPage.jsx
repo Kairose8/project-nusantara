c3bd5d8782af44a2bf74d9f65e6dd74ee0c64794
denjelll <lumenondenzel@gmail.com>
2024-05-13 09:12:21 +0700

    ganti koordinat

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -4,7 +4,7 @@ import { MapContainer, TileLayer } from "react-leaflet";
 export default function MapPage() {
   return (
     <>
-      <MapContainer center={[-6.2562185, 106.6159875, 17]} zoom={15}>
+      <MapContainer center={[-7.796111, 110.366389]} zoom={12}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -14,7 +14,6 @@ export default function MapPage() {
       <p class="font-serif text-5xl mb-5 mt-52 text-white ml-12 w-1/3">
         Adventure Map
       </p>
-      <br />
       <br />
       <br />
       <br />

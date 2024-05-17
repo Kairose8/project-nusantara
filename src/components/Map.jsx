b9fa2501c4dcea12fd19899c3a4ad1e59071da8e
denjelll <lumenondenzel@gmail.com>
2024-05-17 22:38:41 +0700

    tambahan
    ganti nama pop up, bikin grup location

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,47 +1,48 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 export default function Map() {
   const markers = [
     {
       geocode: [-7.7520206, 110.491467], //prambanan
-      popUp: "Hello everybody 1",
+      popUp: "Candi Prambanan",
     },
     {
       geocode: [-7.608, 110.204002], //borobudur
-      popUp: "Hello everybody 2",
+      popUp: "Candi Borobudur",
     },
     {
       geocode: [-7.792333, 110.443333], //goa pindul
-      popUp: "Hello everybody 3",
+      popUp: "Goa Pindul",
     },
     {
       geocode: [-8.033333, 110.433333], //pantai parangtritis
-      popUp: "Hello everybody 4",
+      popUp: "Pantai Parangtritis",
     },
     {
       geocode: [-7.796111, 110.366389], //taman sari
-      popUp: "Hello everybody 5",
+      popUp: "Taman Sari",
     },
     {
       geocode: [-7.797068, 110.370529], //tugu jogja
-      popUp: "Hello everybody 6",
+      popUp: "Tugu Jogja",
     },
     {
       geocode: [-7.811885, 110.383333], //alun alun kidul
-      popUp: "Hello everybody 7",
+      popUp: "Alun Alun Kidul",
     },
     {
       geocode: [-7.799722, 110.382222], //keraton jogja
-      popUp: "Hello everybody 8",
+      popUp: "Keraton Jogja",
     },
     {
-      geocode: [-7.811885, 110.383333], //mendut
-      popUp: "Hello everybody 9",
+      geocode: [-7.5442,110.4333], //merapi
+      popUp: "Gunung Merapi",
     },
     {
       geocode: [-7.793006, 110.365981], //jalan malioboro
-      popUp: "Hello everybody 10",
+      popUp: "Malioboro",
     },
   ];
 
@@ -52,11 +53,14 @@ export default function Map() {
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
+      <MarkerClusterGroup>
+
       {markers.map((marker, index) => (
         <Marker key={index} position={marker.geocode}>
           <Popup>{marker.popUp}</Popup>
         </Marker>
       ))}
+      </MarkerClusterGroup>
     </MapContainer>
   );
 }

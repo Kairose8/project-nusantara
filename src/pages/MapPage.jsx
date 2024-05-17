563e272701f225e47149b8f821514e49e10b79a9
denjelll <lumenondenzel@gmail.com>
2024-05-17 23:45:54 +0700

    rewrite code

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -2,8 +2,10 @@ import Map from "../components/Map";
 
 export default function MapPage() {
   return (
+    
 
     <div className="h-full lg:h-screen">
+      <Map/>
       <br />
       <p class="font-serif text-5xl mb-5 mt-52 text-white ml-12 w-1/3">
         Adventure Map

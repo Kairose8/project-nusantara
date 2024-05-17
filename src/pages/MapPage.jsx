cf9fb0d2cad01d5e472f9a8e44ca114ec8405028
denjelll <lumenondenzel@gmail.com>
2024-05-18 03:20:02 +0700

    ganti nama foto doang

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -5,11 +5,11 @@ export default function MapPage() {
     
 
     <div className="h-full lg:h-screen">
-      <Map/>
       <br />
       <p class="font-serif text-5xl mb-5 mt-52 text-white ml-12 w-1/3">
         Adventure Map
       </p>
+      <Map/>
       <br />
       <br />
       <br />

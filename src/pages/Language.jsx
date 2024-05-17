0ec9f25b15e9eaa300484969bcdbbbd46e98961f
Kairose8 <hafizhdikara@gmail.com>
2024-05-18 04:03:28 +0700

    audeh

--- src/pages/Language.jsx
+++ src/pages/Language.jsx
@@ -1,4 +1,5 @@
 import arrow from "../assets/icons/arrow.png";
+import NavigateBack from "../components/NavigateBack";
 
 export default function Language() {
   return (
@@ -22,7 +23,8 @@ export default function Language() {
             <div className="w-80 md:w-96 mb-5 p-6 bg-myColor-500 border border-myColor-600 rounded-lg">
               <p className="mb-2 text-2xl text-stone-200">Bahasa Indonesia</p>
               <p className="mb-3 text-xs text-stone-300 ">
-              Also known as Indonesian is the country's national language, it would really help to know a few phrases.
+                Also known as Indonesian is the country's national language, it
+                would really help to know a few phrases.
               </p>
               <a
                 href="https://www.udemy.com/course/learn-indonesian-with-inten/?utm_source=adwords&utm_medium=udemyads&utm_campaign=DSA_Catchall_la.EN_cc.ID&campaigntype=Search&portfolio=Indonesia&language=EN&product=Course&test=&audience=DSA&topic=&priority=&utm_content=deal4584&utm_term=_._ag_112265177716_._ad_500831051438_._kw__._de_c_._dm__._pl__._ti_dsa-392130681248_._li_9120718_._pd__._&matchtype=&gad_source=1&gclid=Cj0KCQjw3ZayBhDRARIsAPWzx8rsNtZWv0DkTwJIVXYI_5ZNt9rSAx6gbBB8yXn0kqJfiHtgV1DAHL8aAqx0EALw_wcB&couponCode=LEADERSALE24B"
@@ -39,7 +41,8 @@ export default function Language() {
             <div className="w-80 md:w-96 mb-5 p-6 bg-myColor-500 border border-myColor-600 rounded-lg">
               <p className="mb-2 text-2xl text-stone-200">Boso Jowo</p>
               <p className="mb-3 text-xs text-stone-300 ">
-              known commonly as Javanese, is the language of the natives that reside in Yogyakarta.
+                known commonly as Javanese, is the language of the natives that
+                reside in Yogyakarta.
               </p>
               <a
                 href="https://www.digitaldialects.com/Javanese.htm"
@@ -51,6 +54,7 @@ export default function Language() {
                 <img className="h-6" src={arrow} alt="arrow icon" />
               </a>
             </div>
+            <NavigateBack />
           </div>
         </div>
       </div>

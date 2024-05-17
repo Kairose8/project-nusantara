d387efdc721ff802e8730de631abeb6797da9e1e
Kairose8 <hafizhdikara@gmail.com>
2024-05-18 06:19:41 +0700

    Update ExplorePage.jsx

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -6,10 +6,10 @@ export default function ExplorePage() {
     
       <div className="flex justify-around flex-wrap px-14 h-screen sm:mt-40 md:mt-40">
         <div>
-          <div className="mt-11 font-serif text-xl text-white text-center sm:w-1/3 sm:text-left sm:text-base">
+          <div className="mt-11 font-serif text-xl text-white text-center sm:w-1/3 sm:text-left sm:text-base lg:text-xl">
             <p>Only in</p>
           </div>
-          <div className="font-serif text-5xl font-bold text-white  w-1/3 sm:text-4xl ">
+          <div className="font-serif text-5xl font-bold text-white  w-1/3 sm:text-4xl lg:text-6xl ">
             <p>
               Yogyakarta
             </p>

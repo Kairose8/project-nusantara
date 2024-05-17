6243bdca5c3bfc6d1b06865af8bd09f34dd8d424
Kairose8 <hafizhdikara@gmail.com>
2024-05-18 02:26:21 +0700

    home done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,22 @@
 import GetStarted from "../components/GetStarted";
 
-
 export default function Home() {
-
-
   return (
-    <div className="h-screen ">
-      <p class="font-serif text-5xl mb-5 mt-52 text-white ml-12 w-1/3">
-        The Amazing Yogyakarta
-      </p>
-      <p class="font-serif text-white  md:text-sm ml-12 w-2/3 lg:text-xl lg:w-2/5">
-        Yogyakarta was known with the slogan of "Jogja The Never ending Asia"
-        for its endless appeals. Today, it is also renowned as "Jogja Istimewa
-        ", a tagline that elaborates its special values.
-      </p>
-      <br />
-      <p class="font-serif text-white ml-12 w-1/3">
-        Explore Indonesia’s Special Region!!!
-      </p>
+    <>
+      <div className="h-screen flex flex-col items-center text-center ">
+        <p class="font-serif text-5xl mb-10 mt-52 text-white w-2/3 lg:w-max">
+          The Amazing Yogyakarta
+        </p>
+        <p class="font-grotesk text-white text-justify  md:text-sm w-2/3 lg:text-xl lg:w-3/5">
+          Yogyakarta was known with the slogan of "Jogja The Never ending Asia"
+          for its endless appeals. Today, it is also renowned as "Jogja Istimewa" a tagline that elaborates its special values.
+        </p>
+        <br />
+        <p class="font-grotesk text-white w-1/3">
+          Explore Indonesia’s Special Region!!!
+        </p>
       <GetStarted />
-    </div>
+      </div>
+    </>
   );
 }
--- src/pages/TripPlanner.jsx
+++ src/pages/TripPlanner.jsx
@@ -7,7 +7,7 @@ export default function TripPlanner() {
       <div>
         {/* page text  */}
         <div className="flex flex-col items-center text-center mb-8">
-          <div className="text-3xl md:text-5xl items-center left-14 font-space font-bold text-white md:w-1/3 ">
+          <div className="text-3xl md:text-5xl items-center left-14 font-space font-bold text-white md:w-3/5 lg:w-max ">
             <p>Plan Out Your Trip!</p>
           </div>
           <div className="left-14 font-sans text-lg text-white md:w-1/3 ">

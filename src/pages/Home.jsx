ee6b2aed8122b98b57e8657ade9a013672b1afb7
Kairose8 <hafizhdikara@gmail.com>
2024-05-18 10:08:55 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         <p className="font-serif text-5xl mb-10 mt-52 text-white w-2/3 lg:w-max">
           The Amazing Yogyakarta
         </p>
-        <p className="font-grotesk text-white text-justify  md:text-sm w-2/3 lg:text-xl lg:w-3/5">
+          <p className="font-grotesk text-white text-center  md:text-sm w-2/3 lg:text-xl lg:w-3/5">
           Yogyakarta was known with the slogan of "Jogja The Never ending Asia"
           for its endless appeals. Today, it is also renowned as "Jogja Istimewa" a tagline that elaborates its special values.
         </p>
